76db041698ebbd6ffa7f62ced660f438553437d0
Ramana <itbuzz@gmail.com>
2016-09-08 17:19:51 -0500

    5:15

---
+++ test/src/main/java/test/test/HelloWorld.java
@@ -0,0 +1,25 @@
+package test.test;
+
+public class HelloWorld {
+
+	private String hello;
+	private String world;
+
+	public String getWorld() {
+		return world;
+	}
+
+	public void setWorld(String world) {
+		this.world = world;
+	}
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+	
+	
+}
